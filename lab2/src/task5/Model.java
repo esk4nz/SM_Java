cfd36914cc8ae83cd4d03c9aa7a28d086e210912
esk4nz <grinko27052005@gmail.com>
2025-10-01 23:00:46 +0300

    Working task 5 of lab 2 (changed average load)

--- lab2/src/task5/Model.java
+++ lab2/src/task5/Model.java
@@ -60,7 +60,7 @@ public class Model {
                         + "\nfailure probability = " +
                         p.getFailure() / (double) p.getQuantity()
                         + "\naverage load = " +
-                        p.getAverageLoad(tcurr)); // <-- це правильно!
+                        p.getAverageLoad(tcurr));
             }
         }
     }
