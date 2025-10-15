c8c913cb7d057a0afadb5d29d35570f5c7f1d719
esk4nz <grinko27052005@gmail.com>
2025-10-16 00:37:25 +0300

    Different types of tests

--- lab3/src/task1/SimPriorModel.java
+++ lab3/src/task1/SimPriorModel.java
@@ -38,4 +38,55 @@ public class SimPriorModel {
         Model model = new Model(list);
         model.simulate(1000.0);
     }
+
+
+
+
+
+
+
+//    public static void main(String[] args) {
+//        // Створення процесів
+//        Create c = new Create("CREATOR", 2.0);
+//
+//        Process p1 = new Process("P1", 3.0);
+//        Process p2 = new Process("P2", 6.0);
+//        Process p3 = new Process("P3", 4.0);
+//        Process p4 = new Process("P4", 2.0);
+//
+//        // Налаштування серверів і черг
+//        p1.setServers(2); p1.setMaxqueue(2);
+//        p2.setServers(1); p2.setMaxqueue(1);
+//        p3.setServers(2); p3.setMaxqueue(2);
+//        p4.setServers(1); p4.setMaxqueue(1);
+//
+//        // Розподіл затримки
+//        c.setDistribution("exp");
+//        p1.setDistribution("exp");
+//        p2.setDistribution("exp");
+//        p3.setDistribution("exp");
+//        p4.setDistribution("exp");
+//
+//        // Create обирає куди піти: (наприклад, ймовірності)
+//        c.addNextElement(p1, 0.3);
+//        c.addNextElement(p2, 0.3);
+//        c.addNextElement(p3, 0.4);
+//
+//        // p3 має лише один вихід у p4, без блокування (DROP)
+//        p3.setNextElement(p4); // DROP
+//        p3.setBlockOnFull(false);
+//        // p4 — SINK
+//
+//        ArrayList<Element> list = new ArrayList<>();
+//        list.add(c);
+//        list.add(p1);
+//        list.add(p2);
+//        list.add(p3);
+//        list.add(p4);
+//
+//        Model model = new Model(list);
+//        model.simulate(1000.0);
+//    }
+
+
 }
